a4d47a0b2f937f4153b0603dd29e09df48d893f4
Cristian Camilo García Palacios <134308372+CristianCamiloGarciaPalacios@users.noreply.github.com>
2025-11-19 19:03:03 -0500

    Little fix in router

--- Web frontend/src/router.jsx
+++ Web frontend/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./login.jsx";
 import Home from "./Home.jsx";
 import NotReady from "./Not_ready_page.jsx";
+import Register from "./Register.jsx"; // <-- Importa la página de registro
 
 export const router = createBrowserRouter([
   {
